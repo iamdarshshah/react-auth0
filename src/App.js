41dcca00d48f79205ae21b2719ea2db2b319cf76
iamdarshshah <imdarshshah@gmail.com>
2020-04-28 15:54:13 +0530

    nav and styling added.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Home from "./Home";
 import Profile from "./Profile";
+import Nav from "./Nav";
 
 function App() {
   return (
     <>
-      <Route path="/" exact component={Home} />
-      <Route path="/profile" component={Profile} />
+      <Nav />
+      <div className="body">
+        <Route path="/" exact component={Home} />
+        <Route path="/profile" component={Profile} />
+      </div>
     </>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -3,6 +3,17 @@ import { Link } from "react-router-dom";
 
 export default class Nav extends Component {
   render() {
-    return <div></div>;
+    return (
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+        </ul>
+      </nav>
+    );
   }
 }
